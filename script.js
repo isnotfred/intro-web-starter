6af6d265a027ff35816b3090ae49d8bb82cca257
isnotfred <fredorlain5@gmail.com>
2025-12-19 20:54:54 +0800

    Add JavaScript interaction

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/*
-  TODO:
-  1. Select your button
-  2. Add a click event listener
-  3. Show an alert message when the button is clicked
-
-  Example idea:
-  "Hello! JavaScript is working."
-*/
+document.getElementById("my-button").addEventListener("click", () => {
+  alert("Hello! JavaScript is working.");
+});
